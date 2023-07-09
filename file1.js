a6fd0451faa0e43a9b609ab1c947337b5b910a24
sahilNayakawade <snayakawade7@gmail.com>
2023-07-09 23:23:38 +0530

    v6

--- file1.js
+++ file1.js
@@ -1,2 +1,5 @@
 console.log("this is file1.js from master")
 console.log("this is file1 from dev1")
+console.log("this is file1.js from master")
+console.log("this is file1.js from master")
+console.log("this is file1.js from master")
